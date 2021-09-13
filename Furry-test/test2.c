ae3cfcde87df4d3563197f50968a9cc98133dc1d
omostipan <omostipan@student.ucode.world>
2021-09-13 17:32:09 +0300

    .

--- Furry-test/test2.c
+++ Furry-test/test2.c
@@ -2,14 +2,10 @@
 int main(){
     
     char Full_name[70]; 
-    printf("Введите имя и Фамилию");
-    scanf("%s", &Full_name);
-
-    int age;
-    printf("Введите возраст\n");
-    scanf("%d", &age);
-
-
-
+    printf("Введите имя и Фамилию: ");
+    scanf("%s", Full_name);
+        if("Марина Зяблова" = Full_name){
+            printf("Ты кисонька \n");
+        }
     return 0;
 }

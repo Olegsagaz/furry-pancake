b1f9d97eeb3712d06252244cc03072325926acc4
omostipan <olegm3112@gmail.com>
2021-09-22 23:19:45 +0300

    add all

--- Furry-test/test2.c
+++ Furry-test/test2.c
@@ -1,17 +1,13 @@
  #include <stdio.h>
  #include <unistd.h>
 
-void my_scanfstr()
-{ 
+void my_scanfstr(){ 
     char name[60];
         printf("Введите ваше имя \n");
         printf("Сюда: ");
-        scanf("%s",  &name);
-        sleep (2);
-        //printf("Ваше имя %s  \n", name);
-        if (name == "Марина"){
-            printf("chto to");
-        }            
+        scanf("%s", name);
+        sleep (3);
+        printf("Ваше имя %s  \n", name);          
 }
 
 int main()

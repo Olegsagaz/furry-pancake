9876452148b09d21d2ea0ac7f6d6d63febacccd3
omostipan <omostipan@student.ucode.world>
2021-09-22 16:57:56 +0300

    Chto to

--- Furry-test/test2.c
+++ Furry-test/test2.c
@@ -1,11 +1,21 @@
  #include <stdio.h>
-int main(){
-    
-    char Full_name[70]; 
-    printf("Введите имя и Фамилию: ");
-    scanf("%s", Full_name);
-        if("Марина Зяблова" = Full_name){
-            printf("Ты кисонька \n");
-        }
+ #include <unistd.h>
+
+void my_scanfstr()
+{ 
+    char name[60];
+        printf("Введите ваше имя \n");
+        printf("Сюда: ");
+        scanf("%s",  &name);
+        sleep (2);
+        //printf("Ваше имя %s  \n", name);
+        if (name == "Марина"){
+            printf("chto to");
+        }            
+}
+
+int main()
+{
+    my_scanfstr();
     return 0;
 }
